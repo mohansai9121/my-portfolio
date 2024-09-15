759f16bd54d8b2f86a36698bbb2948e2a6782325
Mohan Sai <mohansaichinthala9121@gmail.com>
2024-09-15 14:26:20 +0530

    Group-Chat added to works

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -6,52 +6,59 @@ import calculator from "./calculator.webp";
 import clock from "./clock.jpg";
 import boxOffice from "./box-office.jpeg";
 import weather from "./weather.jpg";
+import groupChat from "./group-chat.png";
 
 const projects = [
   {
     sNo: "1",
+    name: "Group Chat",
+    img: groupChat,
+    url: "https://group-chat-ce5c0.web.app/",
+  },
+  {
+    sNo: "2",
     name: "Tic Tac Toe Game",
     img: ticTacToe,
     url: "https://mohan-tictactoe-game.netlify.app/",
   },
   {
-    sNo: "2",
+    sNo: "3",
     name: "To Do List",
     img: todolist,
     url: "https://mohansai9121.github.io/to-do-list/",
   },
   {
-    sNo: "3",
+    sNo: "4",
     name: "Password Generator",
     img: password,
     url: "https://password-generation-by-mohansai.netlify.app/",
   },
   {
-    sNo: "4",
+    sNo: "5",
     name: "Crypto Coins",
     img: cryptocoins,
     url: "https://crypto-coins-bymohansai.netlify.app/",
   },
   {
-    sNo: "5",
+    sNo: "6",
     name: "Calculator",
     img: calculator,
     url: "https://calculator-by-mohan.netlify.app/",
   },
   {
-    sNo: "6",
+    sNo: "7",
     name: "Clock",
     img: clock,
     url: "https://mohansai9121.github.io/clock/",
   },
   {
-    sNo: "7",
+    sNo: "8",
     name: "Weather Predictor",
     img: weather,
     url: "https://mohansai9121.github.io/weather-app/",
   },
   {
-    sNo: "8",
+    sNo: "9",
     name: "Box-Office",
     img: boxOffice,
     url: "https://mohansai9121.github.io/box-office/",
