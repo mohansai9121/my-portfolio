8c81d13a638e1bdb55cdaadfb9ea9853300d6fec
Mohan Sai <mohansaichinthala9121@gmail.com>
2024-09-24 01:27:13 +0530

    3d react logo added

--- src/Components/Navbar/Profile/Profile.jsx
+++ src/Components/Navbar/Profile/Profile.jsx
@@ -2,17 +2,38 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import profile from "../../../assets/profile.jpg";
 import "./Profile.css";
+import Reactlogo from "./Reactlogo";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stage } from "@react-three/drei";
 
 const Profile = () => {
   return (
     <div className="profile">
-      <img
-        src={profile}
-        alt="Mohan Sai"
-        title="Mohan Sai"
-        style={{ borderRadius: "100%" }}
-        className="mainProfile"
-      />
+      <div className="logos">
+        <div className="logo">
+          <Canvas>
+            <Stage environment="city" intensity={1}>
+              <Reactlogo />
+            </Stage>
+            <OrbitControls autoRotate autoRotateSpeed={7} enableZoom={false} />
+          </Canvas>
+        </div>
+        <img
+          src={profile}
+          alt="Mohan Sai"
+          title="Mohan Sai"
+          style={{ borderRadius: "100%" }}
+          className="mainProfile"
+        />
+        <div className="logo">
+          <Canvas>
+            <Stage environment="city" intensity={1}>
+              <Reactlogo />
+            </Stage>
+            <OrbitControls autoRotate autoRotateSpeed={7} enableZoom={false} />
+          </Canvas>
+        </div>
+      </div>
       <h1>
         <span className="name">I am Mohan Sai,</span> from Guntur
       </h1>
